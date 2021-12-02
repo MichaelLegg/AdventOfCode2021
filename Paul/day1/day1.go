e77efc37a826d18da720ea98a363f0039ff1add7
BOTPaul <3285385+BOTPaul@users.noreply.github.com>
2021-12-02 19:59:39 +0000

    feat: so I'm a go dev now?

---
+++ Paul/day1/day1.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	s "strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readInput(fileName string) []int {
+	dat, err := os.ReadFile(fileName)
+	check(err)
+
+	stringInput := s.Split(s.Replace(string(dat), " ", "", -1), "\n")
+	intOutput := make([]int, len(stringInput))
+	for i, v := range stringInput {
+		pi, _ := strconv.ParseInt(v, 0, 64)
+		intOutput[i] = int(pi)
+	}
+
+	return intOutput
+}
+
+func countLarger(input []int) int {
+	count := 0
+
+	for i, v := range input {
+		if i == 0 {
+			continue
+		}
+
+		vLast := input[i-1]
+
+		if vLast < v {
+			count = count + 1
+		}
+	}
+
+	return count
+}
+
+func runDay1A() {
+	input := readInput("./input.txt")
+
+	count := countLarger(input)
+
+	fmt.Println("There were", count, "measurements larger than the previous measurement")
+}
+
+func runDay1B() {
+	input := readInput("./input.txt")
+
+	slidingSums := make([]int, len(input))
+
+	for i, v := range input {
+		switch i {
+		case 0, len(input) - 1:
+			slidingSums[i] = v
+		case 1:
+			vLast := input[i-1]
+			slidingSums[i] = vLast + v
+		case len(input) - 2:
+			vNext := input[i+1]
+			slidingSums[i] = vNext + v
+		default:
+			vLast := input[i-1]
+			vNext := input[i+1]
+			slidingSums[i] = vLast + v + vNext
+		}
+	}
+
+	count := countLarger(slidingSums)
+
+	fmt.Println("There were", count, "measurements larger than the previous measurement")
+}
+
+func main() {
+	runDay1A()
+	runDay1B()
+}
---
+++ Paul/day1/go.mod
@@ -0,0 +1,3 @@
+module day1/v1
+
+go 1.17
