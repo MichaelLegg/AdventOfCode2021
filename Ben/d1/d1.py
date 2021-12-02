96f3684b3ee1c9d5c69523a81f0dc0923a1e716d
Ben <bdl@live.co.uk>
2021-12-02 21:14:38 +0000

    ben: d1: p2

--- Ben/d1/d1.py
+++ Ben/d1/d1.py
@@ -2,22 +2,43 @@
 data = []
 with open('d1/input.txt', 'r') as f:
   data = list(map(int, f.readlines()))
-
-inc_count = 0
-
-window = []
-last_sum = 0
-
 data_len = len(data)
 
-for i in range(data_len-2):
+p1_result = 0
+p2_result = 0
+
+window = []
+
+last_data = 0
+last_window_sum = 0
+
+for i in range(data_len):
+
+  # current sample more than last
+  if data[i] > last_data:
+    p1_result += 1
+
+  last_data = data[i]
+
+  # part 2
+  # first window has no previous, so skip
+  if i == 0:
+    continue
+
+  # only run on complete windows
+  if i > (data_len - 3):
+    break
+
+  # create window and sum
   window = [data[i], data[i+1], data[i+2]]
   window_sum = sum(window)
   print(i, window, window_sum)
 
-  if window_sum > last_sum:
-    inc_count += 1
+  # current window sum more than previous window sum
+  if window_sum > last_window_sum:
+    p2_result += 1
 
-  last_sum = window_sum
+  last_window_sum = window_sum
 
-print(inc_count-1)
+print(p1_result)
+print(p2_result)
