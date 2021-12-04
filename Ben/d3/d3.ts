06f8e62c1733bcf595ae5242ae4070719432ce01
Ben <bdl@live.co.uk>
2021-12-04 19:06:38 +0000

    ben: d3: cleanup

--- Ben/d3/d3.ts
+++ Ben/d3/d3.ts
@@ -48,16 +48,15 @@ function p2(data) {
   let rating_o2  = 0;
   let rating_co2 = 0;
 
-  function analyse(data, filter_01) {
+  function analyse(data, filter_for: "o2"|"co2") {
     let remain_data = data
 
     let col = 0;
     while (remain_data.length > 1) {
 
+      // Count number of 0s in column
       let num_0 = 0
       let num_1 = 0
-
-      // Count number of 0s in column
       for (let line of remain_data) {
         num_0 += +(line[col] == 0);
       }
@@ -65,34 +64,34 @@ function p2(data) {
       num_1 = remain_data.length - num_0
 
       // Gamma is 1 if num(1) > num(0) else 0
-      let most_common_bit = 0;
-      if (num_0 == num_1) {
-        most_common_bit = filter_01
+      let match_bit: string;
+      if (filter_for == "o2") {
+        // keep data with bit matching most common bit
+        match_bit = (num_1 >= num_0) ? '1' : '0';
       } else {
-        if (filter_01 == 1) {
-          most_common_bit = +(num_1 > num_0);
-        } else {
-          most_common_bit = +(num_0 > num_1);
-        }
+        // keep data with bit matching least common bit
+        match_bit = (num_1 >= num_0) ? '0' : '1';
       }
 
-      // Keep only data with bit[col] = most common bit
-      remain_data = remain_data.filter((line) => line[col] == most_common_bit)
+      // Keep only data with bit[col] = bit to match
+      remain_data = remain_data.filter((line) => line[col] == match_bit)
       
-      // Move onto next column
+      // Move onto next bit column
       col += 1
     }
+
+    // return last data as integer
     return parseInt(remain_data[0], 2)
   }
 
   // o2 rating is the last remaining element
-  rating_o2 = analyse(data, 1);
-  rating_co2 = analyse(data, 0);
+  rating_o2 = analyse(data, 'o2');
+  rating_co2 = analyse(data, 'co2');
 
   print("O2  rating: %d", rating_o2);
   print("CO2 rating: %d", rating_co2);
-  print(rating_co2);
 
+  // life support rating is o2 * co2 rating
   return rating_o2 * rating_co2;
 }
 
